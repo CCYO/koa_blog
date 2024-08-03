21165a717a0d1d9d3009f766fe7308fdc743f25b
ccy011ccy <ccy011ccy@gmail.com>
2024-08-03 06:03:10 +0000

    調整:src/js/utils/errorHandle.js

--- src/js/utils/errorHandle.js
+++ src/js/utils/errorHandle.js
@@ -10,30 +10,36 @@ window.addEventListener("error", (e) => {
 });
 window.addEventListener("unhandledrejection", function (event) {
   event.preventDefault();
-  dev_log("window.addEventListener(unhandledrejection)捕獲錯誤\nError:\n", e);
   event.promise.catch((result) => {
-    let { errno, _checked } = result;
+    let msg = "window.addEventListener(unhandledrejection)捕獲錯誤事件\n";
+    let { model, _checked } = result;
     if (_checked) {
-      // watchError主動引發，目的為了阻止後續代碼發生連鎖錯誤
-      dev_log(`watchError主動引發`);
-    } else if (errno === FRONTEND._AXIOS.ERR_RES.NO_LOGIN.errno) {
-      // 後端響應請求需要登入權限
-      redir.check_login();
-    } else {
-      // 前端的unhandledrejection(通常發生在異步事件CB內)
-      if (process.env.isProd) {
-        alert("發生未知錯誤，頁面將重新整理");
+      dev_log(
+        `${msg}watchError主動引發，目的為了阻止後續代碼發生連鎖錯誤，故這裡無須再作多餘處理`
+      );
+    } else if (model) {
+      if (!process.env.isProd) {
+        console.error(`error.model:`, model);
+      } else if (model.errno === FRONTEND._AXIOS.ERR_RES.NO_LOGIN.errno) {
+        // 後端響應請求需要登入權限
+        redir.check_login();
       } else {
-        alert("前端代碼錯誤，請確認console");
-        console.error(result);
-        // 需再作回報後端處理
+        location.reload();
       }
+    } else if (!process.env.isProd) {
+      // 以下為前端的unhandledrejection(通常發生在異步事件CB內)
+      alert("前端代碼錯誤，請確認console");
+      console.error(`${msg}Event.reason:`, event.reason);
+    } else {
+      // 以下為前端的unhandledrejection(通常發生在異步事件CB內)
+      alert("發生未知錯誤，頁面將重新整理");
+      location.reload();
+      // 需再作回報後端處理
     }
   });
 });
 
 function watchError(error) {
-  error._checked = true;
   let msg = "發生未知錯誤，頁面將重新整理";
   if (!process.env.isProd) {
     if (error.model) {
@@ -41,13 +47,11 @@ function watchError(error) {
       msg = "伺服器錯誤，請確認console";
     } else {
       // 前端錯誤
+      error._checked = true;
       msg = "前端代碼錯誤，請確認console";
-      console.error(error);
-      // 需再作回報後端處理
     }
   }
   alert(msg);
-  process.env.isProd && location.reload();
   throw error;
 }
 
