0afd57b9a5bce51396949c74c2c2f0b6c2ce34f3
ccy011ccy <ccy011ccy@gmail.com>
2024-08-19 13:43:34 +0000

    調整可讀性

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,16 +5,9 @@ const views = require("./views");
 
 router.use(api.routes());
 router.use(views.routes());
+
 router.get("/", (ctx, next) => {
   ctx.redirect("/square");
-});
-
-let { MyErr } = require("../utils/model");
-router.get("/tt", () => {
-  throw new MyErr({ errno: 8888, msg: "這只是view error測試" });
-});
-router.get("/api/tt", () => {
-  throw new MyErr({ errno: 8888, msg: "這只是api error測試" });
 });
 
 module.exports = router;
