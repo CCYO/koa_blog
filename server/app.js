6524662c55caa2ab8f2c35eec9ef552047c72722
tume20938 <tume20938@gmail.com>
2024-10-05 09:34:21 +0000

    調整:代碼可讀性

--- server/app.js
+++ server/app.js
@@ -1,12 +1,17 @@
-const { consola } = require("consola/basic");
-const { ENV } = require("./config");
+/* NODEJS     ----------------------------------------------------------------------------- */
 ////  NODE.JS MODULE
 const { resolve } = require("path");
+
+/* CONFIG     ----------------------------------------------------------------------------- */
+const { ENV } = require("./config");
+const WEBPACK_CONFIG = require("../build/config");
+const { SESSION_KEY } = require("./_config");
+
+/* NPM        ----------------------------------------------------------------------------- */
 //  設定環境變量，以 ~/.env 作為設定檔
 require("dotenv").config({
   path: resolve(__dirname, `./_config/${ENV.isProd ? ".prod" : ".dev"}.env`),
 });
-
 const Koa = require("koa");
 //  針對JSON類型的response提高可讀性
 const json = require("koa-json")();
@@ -15,10 +20,9 @@ const bodyparser = require("koa-bodyparser")({
   enableTypes: ["json", "form", "text"],
 });
 const koaViews = require("@ladjs/koa-views");
-const koaMount = require("koa-mount");
-const koaStatic = require("koa-static");
+const { consola } = require("consola/basic");
 
-////  MY MODULE
+/* UTILS      ----------------------------------------------------------------------------- */
 //  錯誤處理
 const middleware_errors = require("./middleware/errorsHandle");
 const { webpackDev, webpackHMR } = require("./middleware/webpackDevAndHMR");
@@ -29,24 +33,18 @@ const sequelizeTransaction = require("./middleware/api/seq_transaction");
 //  middleware:ws
 const ws_middleware = require("./middleware/ws");
 const router = require("./routes");
-
-//  避免觸發 ../build/htmlWebpackPlugins.js
-const WEBPACK_CONFIG = require("../build/config");
-const { SESSION_KEY } = require("./_config");
 const { MyErr } = require("./utils/model");
 
+/* RUNTIME    ----------------------------------------------------------------------------- */
 const app = new Koa();
-
 //  加密 session
 app.keys = [SESSION_KEY];
 
 app.use(middleware_errors);
-
 if (!ENV.isProd) {
   //  打印每一次的request與response
   app.use(require("koa-logger")());
 }
-
 app.use(json);
 app.use(webpackDev);
 app.use(webpackHMR);
@@ -54,26 +52,14 @@ app.use(bodyparser);
 app.use(ws_middleware);
 app.use(session_middleware);
 app.use(sequelizeTransaction);
-//  渲染模板
 app.use(
   koaViews(WEBPACK_CONFIG.BUILD.VIEW, {
     extension: "ejs",
   })
 );
-
-//  靜態檔案交由NGINX負責
-// app.use(
-//   koaMount(
-//     WEBPACK_CONFIG.PUBLIC_PATH,
-//     koaStatic(WEBPACK_CONFIG.BUILD.DIST, {
-//       maxage: 60 * 60 * 1000,
-//     })
-//   )
-// );
-
 app.use(router.routes(), router.allowedMethods());
 
-//  列印錯誤
+//  error log
 app.on("error", (error, ctx) => {
   let msg = [
     `----- -----\n
