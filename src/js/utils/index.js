f639c44c335fd9e088d712b556b3ed5c4f110946
tume20938 <tume20938@gmail.com>
2024-10-31 03:33:43 +0800

    調整:webpack build dev與prod的配置2

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -3,24 +3,18 @@ import _Ajv from "./_ajv";
 import Debounce from "./debounce";
 
 /* OBJ        ----------------------------------------------------------------------------- */
-// import render from "./render";
 import _xss from "./_xss";
 import formFeedback from "./formFeedback";
 import redir from "./redir";
-let async_render;
-if (process.env.isProd) {
-  async_render = async () => {
-    let module = await import("./render/index");
-    return module.default;
-  };
-} else {
-  async_render = async () => {
-    let module = await import("./render/dev_index");
-    return module.default;
-  };
-}
+
 /* FUC        ----------------------------------------------------------------------------- */
 import errorHandle from "./errorHandle";
+const async_render = async () => {
+  let module = await import(
+    process.env.isProd ? "./render/index" : "./render/dev_index"
+  );
+  return module.default;
+};
 
 /* EXPORT     ----------------------------------------------------------------------------- */
 export { _Ajv, Debounce, async_render, _xss, formFeedback, redir, errorHandle };
